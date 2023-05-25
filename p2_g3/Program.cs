309e094d9191606aac4f9d905d7991b9aa088adf
Mounia TAHAROUCHTE <mouniataharouchte@gmail.com>
2023-05-25 10:32:40 +0200

    Change display to hello World Master

--- p2_g3/Program.cs
+++ p2_g3/Program.cs
@@ -6,7 +6,7 @@ namespace p2_g3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Master!");
         }
     }
 }
